44175be2362d62de835b47a9dbca008a0b582aef
GasInfinity <sami31275@gmail.com>
2023-06-07 16:38:33 +0200

    Added a class for enum extensions. Bump to 1.1.1
    * Added HasFlag<T>()

---
+++ Rena.Native/Extensions/EnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Rena.Native.Extensions;
+
+public static class EnumExtensions
+{
+    public static unsafe bool HasFlag<T>(this T value, T flag)
+        where T : unmanaged, Enum
+    {
+        if (sizeof(T) == 1)
+            return (Unsafe.As<T, byte>(ref value) | Unsafe.As<T, byte>(ref flag)) == Unsafe.As<T, byte>(ref value);
+        else if (sizeof(T) == 2)
+            return (Unsafe.As<T, short>(ref value) | Unsafe.As<T, short>(ref flag)) == Unsafe.As<T, short>(ref value);
+        else if (sizeof(T) == 4)
+            return (Unsafe.As<T, int>(ref value) | Unsafe.As<T, int>(ref flag)) == Unsafe.As<T, int>(ref value);
+        else // if(sizeof(T) == 8)
+            return (Unsafe.As<T, long>(ref value) | Unsafe.As<T, long>(ref flag)) == Unsafe.As<T, long>(ref value);
+    }
+}
